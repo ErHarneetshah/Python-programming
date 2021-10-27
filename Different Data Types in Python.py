c5b7d6cd1b2c0a390d0682c800528ccade47348c
Harneet shah singh <harneetshahs@gmail.com>
2021-10-28 00:08:07 +0530

    Different types of Data Types

---
+++ Different Data Types in Python.py
@@ -0,0 +1,16 @@
+# Different Types of Data Types in Python
+# Fundamental Data types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+# Classes - Custom Data types
+
+# Specialized Data Types
+
+None
