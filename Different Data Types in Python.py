fb6b9e3bbe1014df641294b214ad61bb3972008d
Harneet shah singh <harneetshahs@gmail.com>
2021-10-28 14:50:51 +0530

    updating

--- Different Data Types in Python.py
+++ Different Data Types in Python.py
@@ -1,7 +1,17 @@
 # Different Types of Data Types in Python
 # Fundamental Data types
+# ..................................................................................
+# 1 Integer i.e. 0,1,-15,10254
 int
+print(2+4)
+print(type(2+4))
+
+# 2 Floating Numbers ie. 0.1, 2.5, 6.9
 float
+print(2.6 + 4.2)
+print(type(2.6 + 4.2))
+
+
 bool
 str
 list
